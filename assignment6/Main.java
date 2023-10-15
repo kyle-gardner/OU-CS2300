58dd945a166e74eb29aa86165662753a7d12637f
kyle-gardner <kyletgardner18@gmail.com>
2023-10-15 02:58:30 -0400

    basics

---
+++ assignment6/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+	// The main function will handle a user's keyboard input specified in the next session
+	public static void main(String[] args) {
+		
+	}
+	// Given arrayLength as argument, create an array of random integers between 0 and 100; return the created array.
+	public static int[] createRandomArray(int arrayLength){
+
+	}
+	// Given an integer array and filename, write the array to the file, with each line containing one integer in the array.
+	public static void writeArrayToFile(int[] array, String filename) {
+
+	}
+	// This is the reverse of writeArrayToFile; Given the filename, read the integers (one line per integer) to an array, and return the array
+	public static int[] readFileToArray(String filename) {
+
+	}
+	// Given an integer array, sort it in-place, i.e., the order of the elements will be changed so that the final array is in sorted order.
+	public static void bubbleSort(int[] array) {
+
+	}
+}
