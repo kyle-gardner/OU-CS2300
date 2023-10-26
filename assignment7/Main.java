e7cf55b557f37815d7c420d2cb26f5d94b39354b
kyle-gardner <kyletgardner18@gmail.com>
2023-10-26 00:57:52 -0400

    bubbleSort, isSorted, and printArray done

--- assignment7/Main.java
+++ assignment7/Main.java
@@ -6,8 +6,11 @@ import java.util.Random;
 public class Main {
 	public static void main(String[] args) {
 		ArrayList<Integer> array1 = makeArray(1000);
-		ArrayList<Integer> array2 = makeArray(1000);
+		ArrayList<Integer> array2 = makeArray(10);
+
+		bubbleSort(array2);
 	}
+
 	public static ArrayList<Integer> makeArray(int length) {
 		ArrayList<Integer> array = new ArrayList<>();
 		Random random = new Random();
@@ -21,10 +24,35 @@ public class Main {
 
 		return array;
 	}
+
 	public static ArrayList<Integer> mergeSort(ArrayList<Integer> array) {
-
+		return array;
 	}
-	public static ArrayList<Integer> bubbleSort(ArrayList<Integer> array) {
 
+	public static ArrayList<Integer> bubbleSort(ArrayList<Integer> array) {
+		for (int i = 0; i < array.size(); i++) {
+			for (int j = 0; j < array.size() - 1; j++) {
+				if (array.get(j) > array.get(j + 1)) {
+					Integer temp = array.get(j + 1);
+
+					array.set(j + 1, array.get(j));
+					array.set(j, temp);
+				}
+			}
+		}
+		return array;
+	}
+	public static boolean isSorted(ArrayList<Integer> array) {
+		for (int i = 0; i < array.size() - 1; i++) {
+			if (array.get(i) > array.get(i + 1)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	public static void printArray(ArrayList<Integer> array) {
+		for (Integer integer : array) {
+			System.out.println(integer);
+		}
 	}
 }
