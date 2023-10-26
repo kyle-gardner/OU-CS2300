38bb515fced9ec95146ab3c32d733e7fd3a23013
kyle-gardner <kyletgardner18@gmail.com>
2023-10-25 23:50:32 -0400

    barebones

---
+++ assignment7/Main.java
@@ -0,0 +1,18 @@
+// implement MergeSort and compare it with BubbleSort using ArrayList<Integer> instead of int []
+// export your Java project to a JAR file.
+import java.util.ArrayList;
+
+public class Main {
+	public static void main(String[] args) {
+
+	}
+	public static ArrayList<String> makeArray() {
+
+	}
+	public static ArrayList<String> mergeSort(ArrayList<String> array) {
+
+	}
+	public static ArrayList<String> bubbleSort(ArrayList<String> array) {
+
+	}
+}
