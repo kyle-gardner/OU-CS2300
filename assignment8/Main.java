ae0b69baa446a66915629c32966297cd5b6d6dfc
kyle-gardner <kyletgardner18@gmail.com>
2023-10-27 16:34:43 -0400

    command line args

--- assignment8/Main.java
+++ assignment8/Main.java
@@ -1,12 +1,19 @@
 public class Main {
+	// idk if he wants all the numbers in the sequence or just the nth fibonaci number so im gonna go with the nth one 
+	// sorry if wrong
 	public static void main(String[] args) {
-		int x = fib(30);
-		System.out.println(x);
+		int n = Integer.parseInt(args[0]);
+		int nth = fib(n);
+
+		System.out.println(nth);
 	}
 
 	public static int fib(int n) {
+		// base
 		if (n == 0) return 0;
 		if (n == 1) return 1;
+
+		// recurse
 		return fib(n-1) + fib(n - 2);
 	}
 }
