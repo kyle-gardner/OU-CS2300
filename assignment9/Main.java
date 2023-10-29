7d3ab357fd360d38516beaa01bbf9b950e399419
kyle-gardner <kyletgardner18@gmail.com>
2023-10-29 02:52:05 -0400

    barebones

---
+++ assignment9/Main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
+
+class Sorter <T extends Comparable<T>> {
+	T obj;
+
+	public Sorter(T obj) {
+		this.obj = obj;
+	}
+
+	public void displayObj() {
+		System.out.println(obj);
+	}
+
+	public T bubbleSort() {
+
+	}
+	public T mergeSort() {
+
+	}
+}
