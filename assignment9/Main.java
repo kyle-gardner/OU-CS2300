4ca84e39eb2851b67b994da99879579c84812d2e
kyle-gardner <kyletgardner18@gmail.com>
2023-10-29 03:59:48 -0400

    think i got generic types right

--- assignment9/Main.java
+++ assignment9/Main.java
@@ -1,12 +1,29 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Random;
 
 public class Main {
 	public static void main(String[] args) {
-		
+		ArrayList<Integer> arr = makeArray(100);
+
+		var sorter = new Sorter<>(arr);
+	}
+		public static ArrayList<Integer> makeArray(int length) {
+		ArrayList<Integer> array = new ArrayList<>();
+		Random random = new Random();
+
+		for (int i = 0; i <= length; i++) {
+			int x = random.nextInt(1000);
+			Integer integer = Integer.valueOf(x);
+
+			array.add(i, integer);
+		}
+
+		return array;
 	}
 }
 
-class Sorter <T extends Comparable<T>> {
+class Sorter <T extends Collection<? extends Comparable>> {
 	T obj;
 
 	public Sorter(T obj) {
@@ -17,10 +34,10 @@ class Sorter <T extends Comparable<T>> {
 		System.out.println(obj);
 	}
 
-	public T bubbleSort() {
-
+	public T  bubbleSort() {
+		return obj;
 	}
 	public T mergeSort() {
-
+		return obj;
 	}
 }
